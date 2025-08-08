de2afed2ff3ef57a0f15774363d181e64314f7b8
pejmansadrin <pejmansadrin@gmail.com>
2025-08-08 18:24:12 +0330

    اصلاح ظاهر کامنت ها

--- src/assets/js/modules/ui.js
+++ src/assets/js/modules/ui.js
@@ -1627,7 +1627,6 @@ const renderComment = (comment) => {
                     <p>${sanitizedContent}</p>
                 </div>
                 <div class="comment-actions">
-                    {/* <<-- تغییر ۲: آیکون پاسخ جایگزین متن "پاسخ" شد -->> */}
                     <button class="btn-text reply-btn" title="پاسخ" ${!state.user ? 'disabled' : ''}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 17 4 12 9 7"></polyline><path d="M20 18v-2a4 4 0 0 0-4-4H4"></path></svg>
                     </button>
