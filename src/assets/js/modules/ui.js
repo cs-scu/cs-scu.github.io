4a315cbae5a21149ff879206be464b60dafad632
pejmansadrin <pejmansadrin@gmail.com>
2025-08-08 18:29:05 +0330

    اصلاح ظاهر کامنت ها

--- src/assets/js/modules/ui.js
+++ src/assets/js/modules/ui.js
@@ -1634,7 +1634,7 @@ const renderComment = (comment) => {
                     <div class="comment-votes">
                         <button class="vote-btn like-comment ${userVote === 1 ? 'active' : ''}" data-vote="1" ${!state.user ? 'disabled' : ''}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
+                                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
                             </svg>
                             <span class="like-count">${comment.likes || 0}</span>
                         </button>
