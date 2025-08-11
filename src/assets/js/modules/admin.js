fc77eab96d75fa44eb2bebd91d802f3f861653f7
pejmansadrin <pejmansadrin@gmail.com>
2025-08-11 21:31:30 +0330

    قابلیت اضافه کردن و ویرایش تگ : تغییر ظاهر مودال تگ ها

--- src/assets/js/modules/admin.js
+++ src/assets/js/modules/admin.js
@@ -465,8 +465,8 @@ const initializeEventsModule = () => {
             tagsListHTML += `
                 <div class="tag-checkbox-item" data-tag-id="${id}">
                     <div class="tag-list-item-actions">
-                        <button class="tag-action-btn edit-tag-btn" title="ویرایش"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></button>
                         <button class="tag-action-btn delete-tag-btn" title="حذف"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></button>
+                        <button class="tag-action-btn edit-tag-btn" title="ویرایش"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></button>
                     </div>
                     
                     <div class="edit-tag-form">
@@ -475,12 +475,11 @@ const initializeEventsModule = () => {
                         <button type="button" class="btn btn-secondary btn-sm cancel-edit-btn">لغو</button>
                     </div>
 
-                    <div class="tag-name-wrapper">
-                        <label for="modal-tag-${id}">${name}</label>
-                        <div class="modal-toggle-switch">
-                            <input type="checkbox" class="toggle-input" id="modal-tag-${id}" value="${id}" ${isChecked ? 'checked' : ''}>
-                            <label for="modal-tag-${id}" class="toggle-label"></label>
-                        </div>
+                    <label class="tag-name" for="modal-tag-${id}">${name}</label>
+                    
+                    <div class="modal-toggle-switch">
+                        <input type="checkbox" class="toggle-input" id="modal-tag-${id}" value="${id}" ${isChecked ? 'checked' : ''}>
+                        <label for="modal-tag-${id}" class="toggle-label"></label>
                     </div>
                 </div>`;
         });
